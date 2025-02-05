50de3d2e6d34f4f8737c082156e26611119cace2
Emanuel Valente <98049322+emavalente@users.noreply.github.com>
2025-02-04 21:28:32 -0300

    Delete api directory

--- api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import server from "./server";
-import dotenv from "dotenv";
-import colors from "colors";
-
-dotenv.config();
-
-const port = process.env.PORT || 4000;
-
-server.listen(port, () => console.log(colors.cyan.bold(`Server is running on port: ${port}`)));
